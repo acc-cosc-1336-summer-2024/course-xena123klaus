e8302e0003311483d043cfd16d06b6ccb60e1ed9
xena123klaus <155030022+xena123klaus@users.noreply.github.com>
2024-06-15 22:16:38 +0000

    solved for homework two.

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,19 @@
-#
+def get_options_ratio(option, total):
+    return option / total
+
+def get_faculty_rating(ratio):
+    if ratio >=.9 and ratio < 1:
+        return "Excellent" 
+    elif ratio > .8:
+        return "Very Good"
+    elif ratio > .7:
+        return "Good"
+    elif ratio > .6:
+        return "Needs Improvement"
+    elif 0 <= ratio <= .59:
+        return "Unacceptable"
+    else:
+        return "Error"
+    
+
+    
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,16 @@
-#
+import decisions 
+
+options_input = input("Enter the value for options: ")
+total_input = input("Enter the value for total: ")
+
+options = float(options_input)
+total = float(total_input)
+
+ratio = decisions.get_options_ratio(options, total)
+
+rating = decisions.get_faculty_rating(ratio)
+
+print(rating)
+
+
+
