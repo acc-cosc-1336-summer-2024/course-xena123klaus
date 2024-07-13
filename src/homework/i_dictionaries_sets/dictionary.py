5567d64df16fe52df11202d59a16c57f2816094f
xena123klaus <155030022+xena123klaus@users.noreply.github.com>
2024-07-13 21:40:41 +0000

    UPDATED

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -14,39 +14,13 @@ def get_p_distance_matrix(list_of_lists):
 
     return matrix
 
-def parse_fasta(fasta_str):
-    sequences = []
-    current_seq = []
-    for line in fasta_str.split('\n'):
-        if line.startswith('>'):
-            if current_seq:
-                sequences.append(current_seq)
-                current_seq = []
-        else:
-            current_seq.extend(list(line.strip()))
-    if current_seq:
-        sequences.append(current_seq)
-    return sequences
 
-def main():
-    while True:
-        print("1-Get p distance matrix")
-        print("2-Exit")
-        choice = input("Enter your choice: ")
 
-        if choice == "1":
-            n = int(input("Enter the number of DNA strings: "))
-            dna_strings = []
-            for i in range(n):
-                dna_string = input("Enter DNA string {}: " .format(i+1))
-                dna_strings.append(list(dna_string.strip()))
 
-            matrix = get_p_distance_matrix(dna_strings)
-            print("P-distance matrix:")
-            for row in matrix:
-                print(' '.join("(:.5f)".format(dist) for dist in row))
-        
-        elif choice == '2':
-            break
-        else:
-            print("Invalid choice, try again.")
+def is_valid_dna_string(dna_string):
+    return all(char in 'ATCG' for char in dna_string)
+
+
+
+
+
--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,3 +1,41 @@
-import dictionary
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
 
-dictionary.main()
+def is_valid_dna_string(dna_string):
+    return all(char in 'ATCG' for char in dna_string)
+
+def main():
+    while True:
+        print("1-Get p distance matrix")
+        print("2-Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            n = int(input("Enter the number of DNA strings: "))
+            dna_strings = []
+            dna_length = None
+            for i in range(n):
+                dna_string = input(f"Enter DNA string {i+1}: ").strip()
+
+                if not is_valid_dna_string(dna_string):
+                    print("Invalid DNA string. DNA strings must contain only 'A', 'T', 'C', 'G'.")
+                    return
+                
+                if dna_length is None:
+                    dna_length = len(dna_string)
+                elif len(dna_string) != dna_length:
+                    print("All DNA strings must be of the same length.")
+                    return
+                dna_strings.append(list(dna_string))
+
+            matrix = get_p_distance_matrix(dna_strings)
+            print("P-distance matrix:")
+            for row in matrix:
+                print(' '.join("{:.5f}".format(dist) for dist in row))
+        
+        elif choice == '2':
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    main()
