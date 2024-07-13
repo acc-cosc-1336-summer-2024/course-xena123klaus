7fefc5ede996839997c401bf1a04f138967f5e05
xena123klaus <155030022+xena123klaus@users.noreply.github.com>
2024-07-13 22:25:52 +0000

    Finished homework 6

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -15,10 +15,45 @@ def get_p_distance_matrix(list_of_lists):
     return matrix
 
 
-
-
 def is_valid_dna_string(dna_string):
     return all(char in 'ATCG' for char in dna_string)
+
+def main():
+    while True:
+        print("1-Get p distance matrix")
+        print("2-Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            n = int(input("Enter the number of DNA strings: "))
+            dna_strings = []
+            dna_length = None
+            for i in range(n):
+                dna_string = input(f"Enter DNA string {i+1}: ").strip()
+
+                if not is_valid_dna_string(dna_string):
+                    print("Invalid DNA string. DNA strings must contain only 'A', 'T', 'C', 'G'.")
+                    return
+                
+                if dna_length is None:
+                    dna_length = len(dna_string)
+                elif len(dna_string) != dna_length:
+                    print("All DNA strings must be of the same length.")
+                    return
+                dna_strings.append(list(dna_string))
+
+            matrix = get_p_distance_matrix(dna_strings)
+            print("P-distance matrix:")
+            for row in matrix:
+                
+                for dist in row:
+                    print("{:.5f}".format(dist), end=' ')
+                print()  
+        
+        elif choice == '2':
+            break
+        else:
+            print("Invalid choice, please try again.")
 
 
 
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,27 @@
-#
+import unittest
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+    def test_p_distance(self):
+        list1 = ['T','T','T','C','C','A','T','T','T','A']
+        list2 = ['G','A','T','T','C','A','T','T','T','C']
+        self.assertAlmostEqual(get_p_distance(list1, list2), 0.4, places=5)
+
+    def test_get_p_distance_matrix(self):
+         lists = [
+            ['T','T','T','C','C','A','T','T','T','A'],
+            ['G','A','T','T','C','A','T','T','T','C'],
+            ['T','T','T','C','C','A','T','T','T','T'],
+            ['G','T','T','C','C','A','T','T','T','A']
+        ]
+         expected_matrix = [
+            [0.0, 0.4, 0.1, 0.1],
+            [0.4, 0.0, 0.4, 0.3],
+            [0.1, 0.4, 0.0, 0.2],
+            [0.1, 0.3, 0.2, 0.0]
+        ]
+         result_matrix = get_p_distance_matrix(lists)
+         for i in range(len(expected_matrix)):
+            for j in range(len(expected_matrix[i])):
+                self.assertAlmostEqual(result_matrix[i][j], expected_matrix[i][j], places=5)
